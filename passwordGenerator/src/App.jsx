7be912690463981c6bf9418bb004262934e8ed0e
JoyDhar32 <joyustcian@gmail.com>
2024-01-27 22:37:04 +1100

    update

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -5,13 +5,21 @@ function App() {
 
   return (
     <>
-    <div className="container mt-5 col-md-6">
-<h2 className="text-center">Generate Your Password</h2>
-<div className="mt-4 p-5 bg-primary text-white rounded">
-
-  
-  </div>
-</div>
+<div className="container mt-5">
+      <h2 className="text-center">Generate Your Password</h2>
+      <div className="row justify-content-center mt-4">
+        <div className="col-md-6 p-5 bg-primary text-white ">
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" />
+            <div className="input-group-append">
+              <button className="btn btn-purple" type="button">
+                Copy
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </>
   )
 }
