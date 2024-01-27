8db01a619ecb015cc3fc4be6c1032e4e2130a237
JoyDhar32 <joyustcian@gmail.com>
2024-01-27 20:24:48 +1100

    installing react app

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,44 +1,34 @@
-import { useState, useCallback, useEffect, useRef } from 'react'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [length, setLength] = useState(8)
-  const [numberAllowed, setNumberAllowed] = useState(false)
-  const [charAllowed, setCharAllowed] = useState(false)
-  const [password, setPassword] = useState('')
-
-  const passwordRef = useRef(null)
-
-  const generatePassword = useCallback(() => {
-    let pass = ""
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-
-    if(numberAllowed) str += "0123456789"
-    if(charAllowed) str += "!@#$%^&*()_+"
-
-    for(let i = 1; i < length; i++) {
-      const char = Math.floor(Math.random() * str.length + 1)
-      pass += str.charAt(char)
-    }
-
-    setPassword(pass)
-
-  }, [length, numberAllowed, charAllowed])
-
-  const copyPasswordToClipboard = () => {
-    window.navigator.clipboard.writeText(password)
-    passwordRef.current?.select()
-    
-  }
-  useEffect(()=>{
-    generatePassword()
-  }, [length, numberAllowed, charAllowed])
+  const [count, setCount] = useState(0)
 
   return (
-  <>
-  
-  
-  </>
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
