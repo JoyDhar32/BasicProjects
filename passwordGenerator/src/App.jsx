f7587dd31a8a002a2c16880e6922026af0d90e2f
JoyDhar32 <joyustcian@gmail.com>
2024-01-29 17:58:09 +1100

    update

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,27 +1,51 @@
-import { useState } from 'react'
+import { useState,useCallback,useEffect, useRef } from "react";
 
 function App() {
-  
-
   return (
     <>
-<div className="container mt-5">
-      <h2 className="text-center">Generate Your Password</h2>
-      <div className="row justify-content-center mt-4">
-        <div className="col-md-6 p-5 bg-primary text-white ">
-          <div className="input-group mb-3">
-            <input type="text" className="form-control" />
-            <div className="input-group-append">
-              <button className="btn btn-purple" type="button">
-                Copy
-              </button>
+      <div className="container mt-5">
+        <h2 className="text-center">Generate Your Password</h2>
+        <div className="row justify-content-center mt-4 ">
+          <div className="col-md-8 p-5 text-white main">
+            <div className="input-group mb-3 passwordColor">
+              <input type="text" className="form-control" />
+              <div className="input-group-append">
+                <button className="btn btn-purple" type="button">
+                  Copy
+                </button>
+              </div>
+            </div>
+            <div className="row">
+              <div class="form-group col-md-5">
+                <input
+                  type="range"
+                  class="form-control-range"
+                  id="passwordLength" 
+                />
+                <label for="passwordLength" > Length</label>
+              </div>
+              <div class="form-group col-md-3">
+                <input
+                  type="checkbox"
+                  class="form-control-range"
+                  id="passwordLength"
+                />
+                 <label htmlFor="number">Numbers</label>
+              </div>
+              <div class="form-group col-md-4">
+                <input
+                  type="checkbox"
+                  class="form-control-range"
+                  id="passwordLength"
+                />
+                <label htmlFor="charInput">Character</label>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
