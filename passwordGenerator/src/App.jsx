65bb9b0065df490e795e7fc70f0b853934816a95
JoyDhar32 <joyustcian@gmail.com>
2024-01-29 18:13:18 +1100

    adding useEffect

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,33 +1,32 @@
-import { useState,useCallback,useEffect, useRef } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  const [length, setLength]=useState(8);
-  const [numberAllowed, setNumberAllowed]=useState(false);
-  const [charAllowed, setCharAllowed]=useState(false);
-  const [password, setPassword]=useState("");
-  const passwordRef=useRef(null);
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+  const passwordRef = useRef(null);
   const generatePassword = useCallback(() => {
-    let pass = ""
-    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    if(numberAllowed) str += "0123456789"
-    if(charAllowed) str += "!@#$%^&*()_+"
+    if (numberAllowed) str += "0123456789";
+    if (charAllowed) str += "!@#$%^&*()_+";
 
-    for(let i = 1; i < length; i++) {
-      const char = Math.floor(Math.random() * str.length + 1)
-      pass += str.charAt(char)
+    for (let i = 1; i < length; i++) {
+      const char = Math.floor(Math.random() * str.length + 1);
+      pass += str.charAt(char);
     }
 
-    setPassword(pass)
-
-  }, [length, numberAllowed, charAllowed])
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed]);
   const copyPasswordToClipboard = () => {
-    window.navigator.clipboard.writeText(password)
-    passwordRef.current?.select() 
-  }
-  useEffect(()=>{
-    generatePassword()
-  }, [length, numberAllowed, charAllowed])
+    window.navigator.clipboard.writeText(password);
+    passwordRef.current?.select();
+  };
+  useEffect(() => {
+    generatePassword();
+  }, [length, numberAllowed, charAllowed]);
 
   return (
     <>
@@ -36,7 +35,7 @@ function App() {
         <div className="row justify-content-center mt-4 ">
           <div className="col-md-8 p-5 text-white main">
             <div className="input-group mb-3 passwordColor">
-              <input type="text" className="form-control" />
+              <input type="text" className="form-control" placeholder="Password" readOnly ref={passwordRef} />
               <div className="input-group-append">
                 <button className="btn btn-purple" type="button">
                   Copy
@@ -48,9 +47,9 @@ function App() {
                 <input
                   type="range"
                   class="form-control-range"
-                  id="passwordLength" 
+                  id="passwordLength"
                 />
-                <label for="passwordLength" > Length</label>
+                <label for="passwordLength"> Length</label>
               </div>
               <div class="form-group col-md-3">
                 <input
@@ -58,7 +57,7 @@ function App() {
                   class="form-control-range"
                   id="passwordLength"
                 />
-                 <label htmlFor="number">Numbers</label>
+                <label htmlFor="number">Numbers</label>
               </div>
               <div class="form-group col-md-4">
                 <input
