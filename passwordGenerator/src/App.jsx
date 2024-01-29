defc3116b08c134b959f797fa367895d08594c07
JoyDhar32 <joyustcian@gmail.com>
2024-01-29 18:01:06 +1100

    adding passwordref

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const [numberAllowed, setNumberAllowed]=useState(false);
   const [charAllowed, setCharAllowed]=useState(false);
   const [password, setPassword]=useState("");
+  const passwordRef=useRef(null);
   return (
     <>
       <div className="container mt-5">
