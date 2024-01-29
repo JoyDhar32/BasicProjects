9141e564836e9392c7c3567441313bd2811d68aa
JoyDhar32 <joyustcian@gmail.com>
2024-01-29 17:58:47 +1100

    updatee

--- passwordGenerator/src/App.jsx
+++ passwordGenerator/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState,useCallback,useEffect, useRef } from "react";
 
 function App() {
+  const [length, setLength]=useState(8);
   return (
     <>
       <div className="container mt-5">
