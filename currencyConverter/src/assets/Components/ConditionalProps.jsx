9d52db3aaf32c2d8f649c9737960dc06eb26d78f
JoyDhar32 <joyustcian@gmail.com>
2024-02-04 09:18:52 +1100

    conditional props

--- currencyConverter/src/assets/Components/ConditionalProps.jsx
+++ currencyConverter/src/assets/Components/ConditionalProps.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 
 const ConditionalProps = (props) => {
-  if (props.login) {
+/*  if (props.login) {
     return <h1>Welcome {props.username}</h1>;
   } else {
     return <h1>Please Login</h1>;
   }
+  */
+
+// return(
+//     <>
+//     {props.login ? <h1>Welcome {props.username}</h1> : <h1>Please Login</h1>}
+//     </>
+// )
+return(props.login? <h1>Welcome {props.username}</h1>:<h1> please Login</h1>)
+
+
+
+
 };
 
 export default ConditionalProps;
