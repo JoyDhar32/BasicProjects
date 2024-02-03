e4b063088e03f0a84242566774e5659e855bfbd3
JoyDhar32 <joyustcian@gmail.com>
2024-02-03 23:53:10 +1100

    update

--- currencyConverter/src/App.jsx
+++ currencyConverter/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const App = () => {
   return (
  <>
+ <img src="https://via.placeholder.com/150" />
  <h1>Hello React</h1>
  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam, nulla?</p>
  </>
