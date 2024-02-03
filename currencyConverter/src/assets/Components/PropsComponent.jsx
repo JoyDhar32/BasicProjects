d9f75ff2cb7861bf9bd35c34a68d22319d0e667d
JoyDhar32 <joyustcian@gmail.com>
2024-02-04 08:24:26 +1100

    props

---
+++ currencyConverter/src/assets/Components/PropsComponent.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const PropsComponent = () => {
+  return (
+    <div>PropsComponent</div>
+  )
+}
+
+export default PropsComponent
+
+/*
+Props: 
+- Full form of props is properties
+- Props are read-only
+- Props are immutable
+- Props allow you to pass data from a parent (wrapping) component to a child (embedded) component.
+- Props are passed to components via HTML attributes.
+- Props are used to pass data, event
+- Props are used to pass data from parent to child component
+
+*/
