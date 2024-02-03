3b67a57c0c7b9c9094c3a579892ba342ccd8dc0e
JoyDhar32 <joyustcian@gmail.com>
2024-02-03 23:56:34 +1100

    update

--- currencyConverter/src/assets/button/Button.jsx
+++ currencyConverter/src/assets/button/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import Style from './Button.module.css'
 const Button = () => {
   return (
-    <div>Button</div>
+    <div className={Style.button}>Button</div>
   )
 }
 
