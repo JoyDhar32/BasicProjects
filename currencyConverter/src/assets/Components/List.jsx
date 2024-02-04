5e15e883f719d3f16799bb11ea499a9cc41f7eff
JoyDhar32 <joyustcian@gmail.com>
2024-02-04 16:05:41 +1100

    Adding css to jsx

--- currencyConverter/src/assets/Components/List.jsx
+++ currencyConverter/src/assets/Components/List.jsx
@@ -7,27 +7,34 @@ const List = () => {
     let numbers= [1,2,3,4,5,6,7,8,9,10];
     numbers.sort((a,b)=>(b-a));
  let output=numbers.map(number=><p>{number}</p>)
-// create an array object of car haing Id , name and model
+// create an array object of car haing Id , name, model and price
 let cars=[
-  {id:1,name:"Toyota",model:"Corolla"},
-  {id:2,name:"Honda",model:"Civic"},
-  {id:3,name:"Suzuki",model:"Mehran"},
-  {id:4,name:"Kia",model:"Sportage"},
-  {id:5,name:"Hyundai",model:"Tucson"},
-  {id:6,name:"Audi",model:"A4"},
-  {id:7,name:"BMW",model:"X5"},
-  {id:8,name:"Mercedes",model:"C-Class"},
-  {id:9,name:"Lamborghini",model:"Aventador"},
-  {id:10,name:"Ferrari",model:"F8 Tributo"}
-]
-let carList = cars.map((car)=><li key={car.id}>CarName: <span style={{color:'purple'}}> {car.name}</span>  &nbsp; Model:<span style={{color:'blue'}}>  {car.model} </span></li>)
+  {id:1,name:"Audi",model:"A4",price:40000},
+  {id:2,name:"BMW",model:"X5",price:50000},
+  {id:3,name:"Mercedes",model:"C200",price:60000},
+  {id:4,name:"Toyota",model:"Corolla",price:20000},
+  {id:5,name:"Honda",model:"Civic",price:25000},
+  {id:6,name:"Suzuki",model:"Alto",price:15000},
+  {id:7,name:"Hyundai",model:"Accent",price:18000},
+  {id:8,name:"Kia",model:"Sportage",price:30000},
+  {id:9,name:"Nissan",model:"Sunny",price:22000},
+  {id:10,name:"Mitsubishi",model:"Lancer",price:23000}
+];
 
+// sort array via name
+cars.sort((a,b)=>(a.name>b.name)?1:-1);
+
+let carList = cars.map((car)=><li key={car.id}>CarName: <span style={{color:'purple'}}> {car.name}</span>  &nbsp; Model:<span style={{color:'blue'}}>  {car.model} </span>&nbsp;   Price: <span style={{color:'red'}}>{car.price} </span></li>)
+let lowPrice=cars.filter((car)=>car.price<=25000);
+let lowPriceCars=lowPrice.map((car)=><li key={car.id}>CarName: <span style={{color:'purple'}}> {car.name}</span>  &nbsp; Model:<span style={{color:'blue'}}>  {car.model} </span> &nbsp; Price:<span style={{color:'red'}}>  {car.price} </span></li>)
   return (
     <>
     <div>Fruits List</div>
 <ol>{ListItems}</ol>
 <p>{output}</p>
 <ol>{carList}</ol>
+<p>Low Price Cars</p>
+<ol>{lowPriceCars}</ol>
     </>
   )
 }
