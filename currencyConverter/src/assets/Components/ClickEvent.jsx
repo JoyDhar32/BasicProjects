1325cbfc3b48f7692196d45d04c4fccfc1c7e14e
JoyDhar32 <joyustcian@gmail.com>
2024-02-04 17:46:14 +1100

    Click Event

--- currencyConverter/src/assets/Components/ClickEvent.jsx
+++ currencyConverter/src/assets/Components/ClickEvent.jsx
@@ -7,13 +7,17 @@ let handleClick = () => {
 let sum = (a, b) => {
     console.log(a + b)
     }
-
+let change=(e)=>{
+    e.target.textContent="Changed Text😊";
+}
 
   return (
     <>
     <div>ClickEvent</div>
     <button onClick={handleClick}>Click me</button>
-    <button onClick={() => sum(2, 3)}>Sum</button>
+    <button onClick={() => sum(2, 3)}>Sum </button> &nbsp;
+    <button  onClick={(e)=>change(e)}>Change</button> 
+
     </>
   )
 }
