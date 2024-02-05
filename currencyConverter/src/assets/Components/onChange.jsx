1731b55f6c312e8057748e0f8895f834ff42ea74
JoyDhar32 <joyustcian@gmail.com>
2024-02-05 15:01:21 +1100

    onChange Event Handler

---
+++ currencyConverter/src/assets/Components/onChange.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+const onChange = () => {
+    const [name,setName] = useState('');
+    const [age,setAge] = useState('');
+    const [address,setAddress] = useState('');
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default onChange
