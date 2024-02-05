e4af15f854298c3d46ba588642bdfdd4197228f5
JoyDhar32 <joyustcian@gmail.com>
2024-02-05 15:42:45 +1100

    onChange Event Handler

--- currencyConverter/src/assets/Components/onChange.jsx
+++ currencyConverter/src/assets/Components/onChange.jsx
@@ -1,33 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 const onChange = () => {
-    const [name,setName] = useState("Guest");
-    const [age,setAge] = useState('');
-    const [address,setAddress] = useState('');
-    const [payment,setPayment] = useState('')
-    const handleNameChange = (e) => {
-        setName(e.target.value);
-    }
-    const handleAgeChange = (e) => {
-        setAge(e.target.value);
-    }
-    const handleAddressChange = (e) => {
-        setAddress(e.target.value);
-    }
-    const handlePayment=(e) =>{
- setPayment(e.target.value);
-    }
+  const [name, setName] = useState("Guest");
+  const [age, setAge] = useState("");
+  const [address, setAddress] = useState("");
+  const [payment, setPayment] = useState("");
+  const [shipping, setShipping] = useState("");
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+  const handleAgeChange = (e) => {
+    setAge(e.target.value);
+  };
+  const handleAddressChange = (e) => {
+    setAddress(e.target.value);
+  };
+  const handlePayment = (e) => {
+    setPayment(e.target.value);
+  };
+  const handleShipping = (e) => {
+    setShipping(e.target.value);
+  };
   return (
     <>
-    <div className="container mt-4">
-    <input type='text' value={name} placeholder='enter you name' onChange={(e)=>handleNameChange(e)} class="form-control"/>
-    <p>Name: {name}</p>
-    <input type='number' value={age} placeholder='enter you age' onChange={(e)=>handleAgeChange(e)}class="form-control" />
-    <p>Age: {age}</p>
-    <textarea value={address} placeholder='enter you address' onChange={(e)=>handleAddressChange(e)} class="form-control"rows="5"  />
-    <p>Address: {address}</p>
-    </div>
+      <div className="container mt-4">
+        <input
+          type="text"
+          value={name}
+          placeholder="enter you name"
+          onChange={(e) => handleNameChange(e)}
+          class="form-control"
+        />
+        <p>Name: {name}</p>
+        <input
+          type="number"
+          value={age}
+          placeholder="enter you age"
+          onChange={(e) => handleAgeChange(e)}
+          class="form-control"
+        />
+        <p>Age: {age}</p>
+        <textarea
+          value={address}
+          placeholder="enter you address"
+          onChange={(e) => handleAddressChange(e)}
+          class="form-control"
+          rows="5"
+        />
+        <p>Address: {address}</p>
+        <select
+          value={payment}
+          onChange={(e) => handlePayment(e)}
+          class="form-control"
+        >
+          <option value="" disabled>
+            Select Your Payment
+          </option>
+          <option value="cash">Cash</option>
+          <option value="card">Card</option>
+          <option value="upi">UPI</option>
+          <option value="giftcard">Gift Card</option>
+        </select>
+        <p> Payment: {payment} </p>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default onChange
+export default onChange;
