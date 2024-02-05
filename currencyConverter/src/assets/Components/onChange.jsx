90c72eb8ec72328af19df7d6abd1dc071e76ff90
JoyDhar32 <joyustcian@gmail.com>
2024-02-05 15:09:31 +1100

    onChange Event Handler

--- currencyConverter/src/assets/Components/onChange.jsx
+++ currencyConverter/src/assets/Components/onChange.jsx
@@ -3,9 +3,23 @@ const onChange = () => {
     const [name,setName] = useState('');
     const [age,setAge] = useState('');
     const [address,setAddress] = useState('');
+    const handleNameChange = (e) => {
+        setName(e.target.value);
+    }
+    const handleAgeChange = (e) => {
+        setAge(e.target.value);
+    }
+    const handleAddressChange = (e) => {
+        setAddress(e.target.value);
+    }
   return (
     <>
     <input type='text' value={name} placeholder='enter you name' onChange={()=>handleNameChange()}/>
+    <p>Name: {name}</p>
+    <input type='text' value={age} placeholder='enter you age' onChange={()=>handleAgeChange()}/>
+    <p>Age: {age}</p>
+    <textarea value={address} placeholder='enter you address' onChange={()=>handleAddressChange()}/>
+    <p>Address: {address}</p>
     </>
   )
 }
