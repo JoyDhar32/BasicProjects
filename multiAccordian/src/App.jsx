85d4dd5b721e9241db99b5f834e019a210779734
JoyDhar32 <joyustcian@gmail.com>
2024-02-09 22:19:30 +1100

    update

--- multiAccordian/src/App.jsx
+++ multiAccordian/src/App.jsx
@@ -1,21 +1,28 @@
 import React, { useState } from "react";
 import Data from "./Data";
+import "./assets/style.css";
 const App = () => {
   const [selected, setSelected] = useState(null);
+  const activeAccordian =(e,getCurrentId)=>{
+setSelected(getCurrentId===selected?null:getCurrentId);
+  }
   return (
     <div className="container">
       <h1 className="text-center mb-4">React Accordian</h1>
       <div className="row text-center">
+        <button className="btn btn-primary btn-sm">Enable MultiSelection</button>
         <div className="accordian">
           {Data && Data.length > 0 ? (
             Data.map((item, index) => {
               return (
                 <div className="accordian-item" key={index}>
-                  <div className="accordian-title">
+                  <div className="accordian-title" onClick={(e)=>activeAccordian(e,item.id)}>
                     <h2>{item.question}</h2>
                   </div>
                   <div className="accordian-content">
-                    <p>{item.answer}</p>
+                    <p>{selected === item.id ? (
+                      <div className="content">{item.answer}</div>
+                    ): null} </p>
                   </div>
                 </div>
               );
@@ -28,5 +35,4 @@ const App = () => {
     </div>
   );
 };
-
 export default App;
