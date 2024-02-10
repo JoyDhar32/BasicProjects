8368f9a4eac699f27b5d245d00f2fe2319f610fa
JoyDhar32 <joyustcian@gmail.com>
2024-02-10 17:22:14 +1100

    ✅ Completed Accordian

--- basic25/src/App.jsx
+++ basic25/src/App.jsx
@@ -1,11 +1,14 @@
 import React, { useState } from "react";
 import Accordian from "./Components/Accordian";
+import RandomColor from "./Components/RandomColor";
+
 
 const App = () => {
 
   return (
   <>
-  <Accordian />
+  {/* <Accordian /> */}
+  <RandomColor />
   </>
   );
 };
